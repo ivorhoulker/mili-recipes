c1c19fcf85d53359aa9dadc88a17e4bda88069bf
Ivor Houlker <ivor.houlker@gmail.com>
2022-11-03 12:07:03 +0800

    tina stuff

---
+++ .tina/config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "recipes",
+        label: "Recipes",
+        path: "/content/recipes",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "subtitle",
+            label: "Subtitle",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "image",
+            label: "Image",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/recipes/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
--- src/app/recipes/[slug]/page.tsx
+++ src/app/recipes/[slug]/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import type { InferGenerateStaticParamsType } from "@type/InferGenerateStaticParamsType";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import client from "../../../../.tina/__generated__/client";
 import clsx from "clsx";
 import { getRecipeBySlug } from "@src/helpers/getRecipeBySlug";
 import { getRecipeParams } from "../../../helpers/getRecipeSlugs";
+import { useTina } from "tinacms/dist/react";
 
 export const revalidate = 10;
 export async function generateStaticParams() {
-  const params = await getRecipeParams();
-  return params.map((x) => ({ slug: x.slug }));
+  const postsListData = await client.queries.recipesConnection();
+  const output = postsListData?.data?.recipesConnection?.edges?.map(
+    (recipe) => ({
+      slug: recipe?.node?._sys.filename,
+    })
+  );
+  return output || [{ slug: "test" }];
 }
 
 export default async function Recipe({
@@ -15,28 +25,39 @@ export default async function Recipe({
 }: {
   params: InferGenerateStaticParamsType<typeof generateStaticParams>;
 }) {
+  const res = await client.queries.recipes({
+    relativePath: `${params.slug}.md`,
+  });
+  const { data } = useTina({
+    query: res.query,
+    variables: res.variables,
+    data: res.data,
+  });
+
   const { slug } = params;
-  const { data, contentHtml } = await getRecipeBySlug(slug);
+  // const { data, contentHtml } = await getRecipeBySlug(slug);
 
   return (
     <main className="z-10 max-w-2xl p-2">
-      <span className="sr-only">{data.title}</span>
+      <span className="sr-only">{data.recipe.title}</span>
       <div className="z-10 py-10">
-        <h1 className="text-6xl font-extrabold text-blue-400">{data.title}</h1>
-        <p className="pt-10 text-xl text-slate-900">{data.subtitle}</p>
+        <h1 className="text-6xl font-extrabold text-blue-400">
+          {data.recipe.title}
+        </h1>
+        <p className="pt-10 text-xl text-slate-900">{data.recipe.subtitle}</p>
       </div>
 
       <figure className="z-10 aspect-video w-full overflow-hidden rounded-xl bg-slate-300 ring-4 ring-purple-400">
         <Image
           className="block w-full -translate-y-1/4 object-cover"
-          alt={data.title}
-          src={`/recipe-images/${data.image}`}
+          alt={data.recipe.title}
+          src={`/recipe-images/${data.recipe.image}`}
           width={3024 / 4}
           height={4032 / 4}
         />
       </figure>
       <div className={clsx("prose prose-slate max-w-2xl")}>
-        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <TinaMarkdown content={data.recipe.body} />
       </div>
     </main>
   );
